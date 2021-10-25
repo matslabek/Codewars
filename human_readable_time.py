0628108a7dc62accb92e013dfba0beb031d4f304
matslabek <61407755+matslabek@users.noreply.github.com>
2021-10-25 19:37:21 +0200

    Create human_readable_time.py

---
+++ human_readable_time.py
@@ -0,0 +1,10 @@
+def make_readable(seconds):
+    hours = seconds // 3600
+    minutes = seconds // 60 - (hours * 60)
+    secs = seconds - (3600 * hours) - (60 * minutes)
+    time_string = ""
+    for item in [hours, minutes, secs]:
+        if item < 10:
+            time_string += "0"
+        time_string += str(item) + ":"
+    return(time_string[:-1])
