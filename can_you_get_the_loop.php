335486390bbb2732248889b77142b883913beb59
matslabek <matslabek@gmail.com>
2022-12-27 12:19:24 +0100

    Update can_you_get_the_loop.php
    Fixed can you get the loop

--- can_you_get_the_loop.php
+++ can_you_get_the_loop.php
@@ -1,26 +1,16 @@
 <?php
 function loop_size(Node $node): int {
-  $checkedNodes = [];
-  $counter = 0;
-  do {
-      $checkedNodes[] = $node;
-      $isFound = array_search($node, $checkedNodes, true);
-        if($counter === 0)
+    $checkedNodes = [];
+    $counter = 0;
+    do {
+        $checkedNodes[] = $node;
+        $node = $node->getNext();
+        $isFound = array_search($node, $checkedNodes, true);
+        $counter++;
+        if ($isFound !== false)
         {
-        //var_dump($node);
-        var_dump($checkedNodes);
-        echo 'CHECKED';
-        var_dump($isFound);
-        echo('FOUND');
+            return $counter - $isFound;
         }
-      $node = $node->getNext();
-      //$isFound = array_search($node, $checkedNodes, true);
-      $counter++;
-      if ($isFound !== false)
-         {
-           return $counter - $isFound; 
-         }
-  } while ($counter <= 10000);
-  echo count($checkedNodes);
-  return 0;
+    } while ($counter <= 10000);
+    return 0;
 }
