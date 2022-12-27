ee651df47a9e6cc27196ac24dd7a28ad0e4f0319
matslabek <matslabek@gmail.com>
2022-12-27 12:30:47 +0100

    Added comments
    Added comment

--- can_you_get_the_loop.php
+++ can_you_get_the_loop.php
@@ -1,16 +1,22 @@
 <?php
+//https://www.codewars.com/kata/52a89c2ea8ddc5547a000863
+//The solution is based on the uniquness of every node
 function loop_size(Node $node): int {
     $checkedNodes = [];
     $counter = 0;
     do {
         $checkedNodes[] = $node;
         $node = $node->getNext();
+		//check if the node has already appeared
         $isFound = array_search($node, $checkedNodes, true);
         $counter++;
+		// if yes, we reached the loop
         if ($isFound !== false)
         {
+			//index of the first node in the loop = length of the dangling piece
+			//thus the loop size can be determined
             return $counter - $isFound;
         }
-    } while ($counter <= 10000);
+    } while (true);
     return 0;
 }
