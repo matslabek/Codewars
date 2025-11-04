3c5545072798ff0901713a6dc3e5eb8d7e616b34
Matslab <matslabek@gmail.com>
2025-11-04 15:29:14 +0100

    - Add All balanced parentheses

--- Go/kata/allBalancedParentheses.go
+++ Go/kata/allBalancedParentheses.go
@@ -1,5 +1,30 @@
 package kata
 
+// https://www.codewars.com/kata/5426d7a2c2c7784365000783/train/go
 func BalancedParens(n int) []string {
-	return nil
+	if n == 0 {
+		return []string{""}
+	}
+
+	result := []string{}
+	generate("", n, 0, 0, &result)
+	return result
+}
+
+func generate(current string, n, open, close int, result *[]string) {
+	// Base case: when we've used all n pairs
+	if len(current) == 2*n {
+		*result = append(*result, current)
+		return
+	}
+
+	// Add opening parenthesis if we haven't used all n
+	if open < n {
+		generate(current+"(", n, open+1, close, result)
+	}
+
+	// Add closing parenthesis if it won't create an invalid sequence
+	if close < open {
+		generate(current+")", n, open, close+1, result)
+	}
 }
--- Go/main.go
+++ Go/main.go
@@ -14,5 +14,27 @@ func main() {
 	//fmt.Println(MoveZeros([]int{1, 2, 0, 1, 0, 1, 0, 3, 0, 1}))
 	//fmt.Println(Alphanumeric(""))
 	//fmt.Println(Cakes(map[string]int{"apples": 500, "sugar": 200, "eggs": 1}, map[string]int{"flour": 1200, "sugar": 1200, "eggs": 5, "milk": 200}))
-	fmt.Println(kata.FirstNonRepeating("bba"))
+	//fmt.Println(kata.FirstNonRepeating("bba"))
+	fmt.Println(kata.BalancedParens(4))
+	sol := []string{"(((())))", "((()()))", "((())())", "((()))()", "(()(()))", "(()()())", "(()())()", "(())(())", "(())()()", "()((()))", "()(()())", "()(())()", "()()(())", "()()()()"}
+	fmt.Println(findDifference(sol, kata.BalancedParens(4)))
+}
+
+func findDifference(a, b []string) []string {
+	bSet := make(map[string]bool)
+
+	// Add all elements from slice B to the map
+	for _, str := range b {
+		bSet[str] = true
+	}
+
+	// Find elements in A that are not in B
+	result := make([]string, 0)
+	for _, str := range a {
+		if !bSet[str] {
+			result = append(result, str)
+		}
+	}
+
+	return result
 }
